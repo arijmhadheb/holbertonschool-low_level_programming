ff43237b1878bd8eaf58a020cb8a455ef7196d23
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 <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>
2024-02-22 10:32:12 +0100

    commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main -Prints "The outcome is a piece of art."
-*
-*Return: Always 0.
-*/
-int main (void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
