9aee1702bce31ae8d785b0966c38cf688b70e66b
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 <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>
2024-02-23 11:43:35 +0100

    commit

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-    char L;
+char L;
 
-    for (L = 'a'; L <= 'z'; L++)
-        putchar(L);
-    
-    for (L = 'A'; L <= 'Z'; L++)
-        putchar(L);
-
-    putchar('\n');
-    return (0);
+for (L = 'a'; L <= 'z'; L++)
+putchar(L);    
+for (L = 'A'; L <= 'Z'; L++)
+putchar(L);
+putchar('\n');
+return (0);
 }
