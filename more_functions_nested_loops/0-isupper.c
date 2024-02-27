e8b37b0ad6d174f852f8ec41290b460ba88e2fb5
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 <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>
2024-02-27 10:37:18 +0100

    commit

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*main - check for uppercase characte.
-*Return: Always 0.
-*/
-int _isupper(int c) 
+ *main - check for uppercase characte.
+ *Return: Always 0.
+ */
+int _isupper(int c)
 
 {
-    if ( c >= "A" && c <= "Z");
-    return(1);
-    else 
-    return(0);
+    if (c >= "A" && c <= "Z")
+        ;
+    return (1);
+    else return (0);
 }
