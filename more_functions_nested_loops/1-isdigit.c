58f90318e183e62e1cb9c46c34a2252129e704e3
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 <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>
2024-02-27 10:51:11 +0100

    commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isdigit - checks for a digit.
+*@c: Checker.
+*Return: Always 0.
+*/
+int _isdigit(int c)
+{ 
+if (c >= '0' && c <= '1');
+return (1);
+else
+return (0);
+}
