5e71cf8f093d6b641383d9bdff6e5f9a6147a255
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 <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>
2024-02-27 11:58:03 +0100

    commit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_most_numbers - Prints numbers
+*Return: void.
+*/
+void print_most_numbers(void);
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (i != 2 && i != 4) 
+_putchar(0);
+}
+_putchar ('\n');
+}
