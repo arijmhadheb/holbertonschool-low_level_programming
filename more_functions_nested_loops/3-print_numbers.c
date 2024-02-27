3a94e7d9a215dc657f732c2d6c4e40d4b021f050
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 <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>
2024-02-27 11:25:14 +0100

    commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print - Prints the number.
+*@c: Caracter
+* Return: Always 0.
+*/
+void print_numbers(void)
+{
+    char c;
+    for ( c = '0' ; c <= '9' , c++ );
+    { 
+        _putchar(char c);
+    }
+    _putchar('\n');
+}
