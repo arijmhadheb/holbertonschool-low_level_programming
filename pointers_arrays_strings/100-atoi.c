023aad9d43c231bc356e06dcf284d5ed06323774
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 <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>
2024-03-01 11:03:03 +0100

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,24 @@
 #include "main.h"
+
 /**
- *_atoi - convert a string to an integer.
- *@s: the string.
- * * Return: The integer
+ * _atoi - Converts a string to an integer.
+ * @s: The string .
+ * Return: The integer.
  */
 int _atoi(char *s)
 
 {
-	int L = 1;
+	int i = 1;
 	unsigned int n = 0;
 	do
 	{
 		if (*s == '-')
-			L *= -1;
+			i *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	} while (*s++);
 
-	return (n * L);
+	return (n * i);
 }
