58148eb9893dbd82950cca26bdc837f11c3185f0
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 <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>
2024-03-01 21:57:29 +0100

    commit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates the string.
- *@dest: destination
- *@src: pointer
- *Return: void
+ * _strcat - concatenate two strings
+ * @dest : pointer
+ * @src : pointer 2
+ * Return: Always 0.
+ * Credit : oussema turki
  */
 char *_strcat(char *dest, char *src)
 {
 	int a, b;
 
-	for (a = 0, dest[a] != '\0'; a++)
-
-		for (b = 0, src[b] != '\0'; b++)
-		{
-			dest[a + b] = src[b];
-		}
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
 	return (dest);
 }
