f3d0c500648d7ea37b198417f4b8e586eb9784c3
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 <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>
2024-03-04 19:49:00 +0100

    commit

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strspn - the function that gets the length of a prefix substring.
+ *@s: parameter
+ *@accept: parameter
+ *Return: Always success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[i] != accept[j]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
+}
