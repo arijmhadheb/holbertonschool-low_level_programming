6a3029473b447322d163281a46e10e39347b22c0
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 <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>
2024-03-04 12:55:50 +0100

    commit

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memset - fills memory with a constant byte.
+ *@s: pointer of the area.
+ *@b: constant byte.
+ *@n: number of byte.
+ *Return: pointer to the memory area.
+ */
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int count;
+	char *p = s;
+
+	for (count = 0; count < 0; count++)
+	{
+		p[count] = b;
+	}
+	return (p);
+}
