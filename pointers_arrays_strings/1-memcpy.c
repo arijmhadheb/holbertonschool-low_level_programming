0ad34d97626585de8cf1dd62012d3332ae13ddf1
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 <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>
2024-03-04 13:15:14 +0100

    commit

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memcpy - a function that copies memory area.
+ *@src: source of the memory area.
+ *@dest: destination of the memory area.
+ *@n: number of bytes.
+ *Return: pointer to destination.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+    return(dest);
+}
