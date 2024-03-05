bc0a0e3dad13770d4546fd75b5a89cae1229cb9d
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 <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>
2024-03-05 15:38:43 +0100

    commit

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,8 @@
  *_strpbrk - the function that searches a string for any of a set of bytes.
  *@s: parameter
  *@accept: parameter
- *Retur :pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found.
+ *Return: pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
