697e79286b7d6f2416c394cda1fea25945bc498d
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 <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>
2024-03-05 15:34:36 +0100

    commit

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -11,11 +11,13 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 
 	while (*s)
+	{
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 				return (s);
-			s++;
 		}
+		s++;
+	}
 	return (NULL);
 }
