d8c6b1e4959cf70ccc69b245c8dbc30b6978b098
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 <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>
2024-03-09 14:49:44 +0100

    commit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,9 +5,11 @@
  * @n: number is squared and compared against base
  * @b: base number to check
  * Return: natural square root of number base
- * _sqrt_recursion - return the natural square root of a number n.
- * @n: number to check for square roots.
- * Return: the natural square root of number n
+ * _sqrt_recursion - Calculates the natural square 
+ * root of a number recursively.
+ * @n: The number for which the square root is to be calculated.
+ * Return: The natural square root of the number 'n', or -1 if 'n' does not
+ * have a natural square root.
  */
 int helper(int n, int b)
 {
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * int is_prime_number - returns if the number is a prime number
+ * @n: the number to check
+ * Return:the input 
+ */
