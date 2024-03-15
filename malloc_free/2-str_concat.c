9c6bd9670489c5206d4891908d9365495fac634f
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 <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>
2024-03-15 13:11:44 +0100

    commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,35 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * str_concat - concatenate two strings using malloc
- * @a: string 1
- * @b: string 2
+ * @x: string 1
+ * @y: string 2
  * Return: pointer to concat string
  */
-char *str_concat(char *a, char *b)
+
+char *str_concat(char *x, char *y)
 {
-    char *c;
-    int i, j, z, x;
+	char *b;
+	int i, j, c, d;
 
-    if (a == NULL)
-        a = "";
-    if (b == NULL)
-        b = "";
-    for (i = 0; a[i] != '\0'; i++)
-        ;
-    for (j = 0; b[j] != '\0'; j++)
-        ;
-    c = malloc((i * sizeof(*a)) + (j * sizeof(*b)) + 1);
-    if (c == NULL)
-        return (NULL);
+	if (x == NULL)
+		y = "";
+	if (x == NULL)
+		y = "";
 
-    for (z = 0, x = 0; *c < (i + j + 1); c++)
-    {
-        if (*c < i)
-            c[z] = a[z];
-        else
-            c[z] = b[x++];
-    }
+	for (i = 0; x[i] != '\0'; i++)
+		;
+	for (j = 0; y[j] != '\0'; j++)
+		;
 
-    return (c);
+	b = malloc((i * sizeof(*x)) + (j * sizeof(*y)) + 1);
+	if (b == NULL)
+		return (NULL);
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			b[c] = x[c];
+		else
+			b[c] = y[d++];
+	}
+
+	return (b);
 }
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,4 +1,4 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
-char *str_concat(char *a, char *b);
+char *str_concat(char *x, char *y);
 
