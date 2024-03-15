f83d9a41bf6300d6759472f293216b8462fcd6a0
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 <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>
2024-03-15 14:33:14 +0100

    commit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - a funtion that frees a 2d grid.
+ * @grid: a pointer of 2d grid
+ * @height: a height of grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,3 +2,4 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *x, char *y);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
