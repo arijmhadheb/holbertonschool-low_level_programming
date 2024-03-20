9e9163407edcb003a90c48d499a48cc7a7de1525
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 <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>
2024-03-20 14:36:04 +0100

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 /**
- * print_name - a function that print a name.
- * @name: name
- * Return: Nothing
+ * print_name - Print a name from a function pointer
+ * @name: a string
+ * @f: function pointer.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+f(name);
 }
