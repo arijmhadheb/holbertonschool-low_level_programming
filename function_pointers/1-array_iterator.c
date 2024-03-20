c3c2fb02e392e583c0aca7ebc24eafd2f4f0038d
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 <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>
2024-03-20 15:41:47 +0100

    commit

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - a function that executes a given function.
+ * @array: a pointer to the first element of the array.
+ * @size: the size of the array
+ * @action: the pointer that takes an integer parameter
+ * Return: a given function excutable
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
