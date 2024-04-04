38681a1ef5f0c43f2bbbb2f8b412b2409a546b53
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 <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>
2024-04-04 18:28:17 +0200

    commit

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t count;
-    count = 0;
+	size_t count;
+	count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        count++;
-    }
-    return(count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
