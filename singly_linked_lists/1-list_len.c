ed65fece53a6fb333214c728df83f3c6c7da3a67
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 <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>
2024-04-04 18:26:57 +0200

    commit

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+size_t list_len(const list_t *h)
+{
+    size_t count;
+    count = 0;
+
+    while (h != NULL)
+    {
+        h = h->next;
+        count++;
+    }
+    return(count);
+}
