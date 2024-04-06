f27108ffb6ff255ca3cab8836083a35c9319b389
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 <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>
2024-04-06 12:37:43 +0200

    commit

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
  * dlistint_len - returns the number of elements in a linked dlistint_t list.
- * @h: the pointer of the head of the node 
+ * @h: the pointer of the head of the node
  * Return :number of the elements
-*/
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h != NULL)
-    {
-        len++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
 
-    return len;
+	return len;
 }
