a6168e03ba594178ff5d70a13c9653d4984935d2
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 <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>
2024-04-26 20:40:25 +0200

    commit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * helper - checks the input number from n to the base
+ * checker - checks the input number from n to the base
  * @n: number is squared and compared against base
- * @b: base number to check
+ * @base: base number to check
  * Return: natural square root of number base
- *_sqrt_recursion - Calculates the natural square
- * root of a number recursively.
- * @n: The number for which the square root is to be calculated.
- * Return: The natural square root of the number 'n', or -1 if 'n' does not
- * have a natural square root.
+ * _sqrt_recursion - return the natural square root of a number n.
+ * @n: number to check for square roots.
+ * Return: the natural square root of number n
  */
-int helper(int n, int b)
+int checker(int n, int base)
 {
-	if (n * n == b)
+	if (n * n == base)
 		return (n);
-	if (n * n > b)
+	if (n * n > base)
 		return (-1);
-	return (helper(n + 1, b));
+	return (checker(n + 1, base));
 }
+
 int _sqrt_recursion(int n)
 {
-	return (helper(1, n));
+	return (checker(1, n));
 }
